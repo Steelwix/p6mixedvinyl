8243671a04ae321c60eb2a75aef1c0657a2ede94
Steelwix <mhunmael@hotmail.com>
2022-06-14 17:16:27 +0200

    Chapter 7 twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\UnicodeString;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['artist' => 'Machine Gun Kelly, glaive', 'song' => 'more than life'],
+            ['artist' => 'GG Magree, Royal & The Serpent', 'song' => 'BITCH'],
+            ['artist' => 'Rival', 'song' => 'Oblivion'],
+            ['artist' => 'Electric Callboy', 'song' => 'Hate/Love'],
+            ['artist' => 'Grabbitz', 'song' => 'Die For You']
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
